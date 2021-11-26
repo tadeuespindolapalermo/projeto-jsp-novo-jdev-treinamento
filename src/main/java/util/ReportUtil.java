2e08e554dfa340a9359066b526b3d3d5b76c7a6a
Tadeu Espíndola Palermo <tadeupalermoti@gmail.com>
2021-11-25 23:38:32 -0300

    Construção do relatório e classe utilitária para geração de PDF.

---
+++ src/main/java/util/ReportUtil.java
@@ -0,0 +1,25 @@
+package util;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.List;
+
+import jakarta.servlet.ServletContext;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+
+public class ReportUtil implements Serializable {
+
+	private static final long serialVersionUID = -7392526883009869201L;
+
+	public byte[] gerarRelatorioPDF(List<?> dados, String nome, ServletContext context) throws JRException {
+		var dataSource = new JRBeanCollectionDataSource(dados);
+		var pathReport = context.getRealPath("relatorio") + File.separator + nome + ".jasper";
+		var printJasper = JasperFillManager.fillReport(pathReport, new HashMap<>(), dataSource);
+		return JasperExportManager.exportReportToPdf(printJasper);
+	}
+
+}
